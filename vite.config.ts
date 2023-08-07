985fc527a06f300364578b4c65232bcd928e6ac5
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-06 21:33:41 -0300

    fixed

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@/components": path.resolve(__dirname, "src/components"),
-      "@/enums": path.resolve(__dirname, "src/enums"),
-      "@/hooks": path.resolve(__dirname, "src/hooks"),
-      "@/interfaces": path.resolve(__dirname, "src/interfaces"),
-      "@/pages": path.resolve(__dirname, "src/pages"),
-      "@/schemas": path.resolve(__dirname, "src/schemas"),
-      "@/styles": path.resolve(__dirname, "src/styles"),
-      "@/utils": path.resolve(__dirname, "src/utils"),
+      '@/components': path.resolve(__dirname, 'src/components'),
+      '@/enums': path.resolve(__dirname, 'src/enums'),
+      '@/hooks': path.resolve(__dirname, 'src/hooks'),
+      '@/interfaces': path.resolve(__dirname, 'src/interfaces'),
+      '@/pages': path.resolve(__dirname, 'src/pages'),
+      '@/schemas': path.resolve(__dirname, 'src/schemas'),
+      '@/styles': path.resolve(__dirname, 'src/styles'),
+      '@/utils': path.resolve(__dirname, 'src/utils'),
     },
   },
-});
-
+})
