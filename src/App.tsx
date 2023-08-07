638efa032219357c83388425dc38b0dff7ea1202
Gesiel Neves <neves.gesiel@gmail.com>
2023-08-06 22:02:09 -0300

    feat/ add button material ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Header from "@/components/Header";
+import { useState } from 'react'
+
+import reactLogo from './assets/react.svg'
+
+import viteLogo from '/vite.svg'
+
+import './App.css'
+import Header from '@/components/Header'
+
+import { Button } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -20,18 +25,24 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <Button onClick={() => setCount((count) => count + 1)}>
+          count is 
+          {' '}
+          {count}
+        </Button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit 
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
